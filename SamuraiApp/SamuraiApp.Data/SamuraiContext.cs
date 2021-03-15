dcea3216f449ae9f7fe37b686a3688b1b5c1948a
Alex Fuller <fulleralex47@gmail.com>
2021-03-15 07:29:21 -0400

    Initial Build. Created EF Core proj using .Net 5.0. Created a data, domain, and UI layer. Created local DB and populated 2 rows of Samurai table.

---
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        /// <summary>
+        /// Now required to tell the app specifically what to connect to. Abstracted connections are now gone.
+        /// </summary>
+        /// <param name="optionsBuilder">Default Db context options builder, defines db connection properties.</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData"); //How to connect to sql server
+        }
+    }
+}
---
+++ SamuraiApp/SamuraiApp.UI/Program.cs
@@ -0,0 +1,44 @@
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+
+        private static void Main(string[] args)
+        {
+            _context.Database.EnsureCreated();
+            GetSamurais("Before");
+            AddSamurai();
+            GetSamurais("After");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Add a new Samurai named "Sampson" to the Samurai DB
+        /// </summary>
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Alex" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Get all Samurai from the DB returning their count and names
+        /// </summary>
+        /// <param name="text">Custom text to put befroe the samurai count</param>
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach(var samurai in samurais)
+                Console.WriteLine(samurai.Name);
+        }
+    }
+}
